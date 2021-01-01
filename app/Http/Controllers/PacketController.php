548b56f365238ffe576d59900158b27bd138a790
adi saputra <adii.empty@gmail.com>
2021-01-01 14:36:15 +0800

    api_ send email with google smtp fix

--- app/Http/Controllers/PacketController.php
+++ app/Http/Controllers/PacketController.php
@@ -4,9 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+
 use App\Models\User;
 use App\Models\Customer;
 use App\Models\Packet;
+use App\Mail\SendMail;
 
 class PacketController extends Controller
 {
@@ -47,7 +51,7 @@ class PacketController extends Controller
 
           //get customer email
           $customer_email = Customer::findOrFail($customer_id)->email;
-          
+
           $packet = Packet::create([
               'name' => $request->name,
               'ket' => $request->ket,
@@ -60,7 +64,12 @@ class PacketController extends Controller
           ]);
 
           if ($packet) {
-              return $this->successResponse($packet, 'Pakcet successfully added', 201);
+              $sending_email = $this->sendEmail();
+              if($sending_email) {
+                  
+                  return $this->successResponse($packet, 'Pakcet successfully added, sending auto email success', 201);      
+              }
+              
           }
 
       }
@@ -79,6 +88,78 @@ class PacketController extends Controller
             // 'customer_id' => 'required'
         ]);
     }
+
+    /**
+     * send email
+     * percobaan
+     */
+    public function sendEmail()
+    {
+        $user_id = auth()->user()->id;
+        //get customer id. fetch all customer. but now we doing it manually by assigning it ehehe
+        $customer_id = 20;
+
+        //get customer email
+        $customer_email = Customer::findOrFail($customer_id)->email;
+
+        $title = '[Pemberitahuan] Paket Untuk Anda [Pemberitahuan]';
+        //customer detail
+        $customer_detail = Customer::findOrFail($customer_id);
+
+        //packet detail
+        $packet_detail = DB::table('packets')
+            ->where('customer_id', '=', $customer_id)
+            ->where('email_status', '=', '0')
+            ->get();
+        
+        $sendmail = Mail::to($customer_email)->send(new SendMail($title, $customer_detail, $packet_detail));
+
+        if (empty($sendmail)) {
+            /**
+             * set email status to 1 
+             */
+            $updated_email_status = DB::table('packets')
+                ->where('customer_id', '=', $customer_id)
+                ->where('email_status', '=', '0')
+                ->update(['email_status' => '1']);
+
+            return response()->json([
+                'message' => 'Mail Sent Sucssfully'
+            ], 200);
+        }else{
+            return response()->json([
+                'message' => 'Mail Sent fail'
+            ], 400);
+        }
+    }
+
+    /**
+     * ambil packet
+     * update status to 1 if packet was taken by customer
+     */
+    public function setPacketStatus(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'id' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return $this->errorResponse($validator->messages(), 422);
+        }
+
+        /**
+         * update status diambil apa tidak
+         */
+        $update_status = DB::update('update packets set status = 1 where id = ?', [$request->id]);
+        $detailed_packet = Packet::findOrFail($request->id);
+
+        return response()->json([
+            'message' => 'Data updated successfully, packet status sudah diambil',
+            'data' => $detailed_packet
+        ]);
+
+    }
+
     /**
      * test`
      */
